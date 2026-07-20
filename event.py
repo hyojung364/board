db6fe3714d004e66402e8661e78184b7e9bef677
hyojung364 <aa01024724718@gmail.com>
2026-07-20 18:19:37 +0900

    Refactor event display and integrate calendar
    Updated event details and added a calendar feature.

--- event.py
+++ event.py
@@ -3,12 +3,13 @@ import pandas as pd
 
 st.set_page_config(layout="wide")
 
-col1, col2, = st.columns([1,2])
+
+col1, col2, = st.columns([1,6])
 with col1:
     st.image('C:\\Users\\user\\Desktop\\강.jpg', width=250)
 
 with col2:
-    st.title("인화여고 도서관 공식 행사 안내")
+    st.title("인화여자고등학교 도서관 공식 행사 안내")
 
 left, right = st. columns([7,3])
 with left:
@@ -21,22 +22,42 @@ with right:
     st.button("행사 검색")
 
 
+from streamlit_calendar import calendar
+
 with st. container():
-    con1, con2, con3 = st.columns([2,1,1])
+    con1, con2, con3, con4 = st.columns([2,1,3,2])
+
     with con1:
         st.subheader("여름맞이 책표지 창작 공모전")
-        st.write("📅2026-07-16")
+        st.write("📅2026-07-07~2026-07-16")
     with con2:
         st.write("📅2026-07-07, 학생회")
     with con3:
         st.image('C:\\Users\\user\\Desktop\\독.jpg', width= 90)
+        
+    with con4:
+        calendar(
+            options={
+            "initialView": "dayGridMonth",
+            "headerToolbar": {
+                "left": "prev,next",
+                "center": "title",
+                "right": ""
+            },
+            "height": 400, 
+        },
+        key="calendar"
+    )
+
+
+
 
 
 with st. container():
-    f1, f2, f3 = st.columns([2,1,1])
+    f1, f2, f3 = st.columns([2,1,3])
     with f1:
         st.subheader("여름방학 독서 캠페인")
-        st.write("📅2026-07-17")
+        st.write("📅2026-07-10~2026-07-17")
     with f2:
         st.write("📅2026-07-10, 도서부")
 
@@ -50,7 +71,6 @@ with st. container():
 with st. container():
     st.subheader("(종료)점심시간 캘리그라피 이벤트")
     st.write("📅2026-07-7")
-
 
 
 st.pagination(10)
